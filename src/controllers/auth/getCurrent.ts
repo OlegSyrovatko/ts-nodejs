eaefc4cfae4b6a3991e31b33a386a70597a32e04
Oleg <sirov@ukr.net>
2024-07-16 19:29:19 +0300

    add tokenRefresh to current

--- src/controllers/auth/getCurrent.ts
+++ src/controllers/auth/getCurrent.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import ctrlWrapper from "../../helpers/ctrlWrapper";
 import { IUser, User } from "../../models/user";
 import HttpError from "../../helpers/HttpError";
+import { Token } from "../../models/user";
 
 export const getCurrent = ctrlWrapper(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -17,12 +18,19 @@ export const getCurrent = ctrlWrapper(
       throw HttpError(404, "User not found");
     }
 
+    const tokenDoc = await Token.findOne({ email: user.email });
+
+    if (!tokenDoc) {
+      throw HttpError(401, "Refresh token not found");
+    }
+
     res.json({
       email: user.email,
       subscription: user.subscription,
       name: user.name,
       avatarURL: user.avatarURL,
       movieIds: user.movieIds,
+      tokenRefresh: tokenDoc.tokenRefresh,
     });
   }
 );
